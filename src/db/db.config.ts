0a4c9cce5318ab8040ff28400f6bc3529946dfdf
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-11-29 15:19:32 +0530

    PrismaClient

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient({
-  log: ["query"],
+// Initialize Prisma client using the environment variable
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: process.env.DATABASE_URL, // Use the DATABASE_URL from environment variables
+		},
+	},
 });
 
 export default prisma;
