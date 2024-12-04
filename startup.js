ea271e2f5a1c3d0d1b836c748f69de170a0c91da
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-12-04 20:51:52 +0530

    "Removed startup.js file, updated web.config to point to dist/index.js, and made changes to .github/workflows/main_campuserp.yml and tsconfig.json files."

--- startup.js
+++
@@ -1,9 +0,0 @@
-
-const path = require('path');
-
-try {
-  require(path.join(__dirname, 'dist', 'index.js'));
-} catch (error) {
-  console.error('Error starting application:', error);
-  process.exit(1);
-}
