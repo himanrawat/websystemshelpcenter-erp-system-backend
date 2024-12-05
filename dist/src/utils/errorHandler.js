c4123937dbca13d659b35c934cbd170c91d9387e
Himanshu Rawat <officialhimanshurawat@gmail.com>
2024-12-05 11:16:02 +0530

    removed dist folder and updated github work flow

--- dist/src/utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import { Response } from "express";
-// export const handleInternalServerError = (error: Error, res: Response, message: string) => {
-//   console.error(`${message}:`, error);
-//   res.status(500).json({ message: "Internal server error" });
-// };
